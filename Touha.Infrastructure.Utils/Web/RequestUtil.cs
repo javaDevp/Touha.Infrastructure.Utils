cb8ffd6254dff797b717eea684283e6a81ff6d24
javaDevp <1129597198@qq.com>
2017-02-07 10:01:54 +0800

    为RequestUtil新增方法,以应对客户端ipv6的问题

--- Touha.Infrastructure.Utils/Web/RequestUtil.cs
+++ Touha.Infrastructure.Utils/Web/RequestUtil.cs
@@ -22,6 +22,41 @@ namespace Touha.Infrastructure.Utils.Web
                 return HttpContext.Current.Request.ServerVariables.Get("Remote_Addr").ToString().Trim();
         }
 
-        
+        /// <summary>
+        /// 获取客户端IP地址（IPV4）
+        /// </summary>
+        /// <returns></returns>
+        public static string GetClientIPv4()
+        {
+            string ipv4 = string.Empty;
+
+            foreach (IPAddress ip in Dns.GetHostAddresses(GetClientIP()))
+            {
+                if (ip.AddressFamily.ToString() == "InterNetwork")
+                {
+                    ipv4 = ip.ToString();
+                    break;
+                }
+            }
+
+            if (ipv4 != string.Empty)
+            {
+                return ipv4;
+            }
+
+            // 原作使用 Dns.GetHostName 方法取回的是 Server 端資訊，非 Client 端。
+            // 改寫為利用 Dns.GetHostEntry 方法，由獲取的 IPv6 位址反查 DNS 紀錄，
+            // 再逐一判斷何者屬 IPv4 協定，即可轉為 IPv4 位址。
+            foreach (IPAddress ip in Dns.GetHostEntry(GetClientIP()).AddressList)
+            {
+                if (ip.AddressFamily.ToString() == "InterNetwork")
+                {
+                    ipv4 = ip.ToString();
+                    break;
+                }
+            }
+
+            return ipv4;
+        }
     }
 }
