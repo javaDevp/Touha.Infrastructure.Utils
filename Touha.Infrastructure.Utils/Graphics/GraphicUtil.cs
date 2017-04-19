a558230090ac79b1e5772521eb091ba15ad9962c
javaDevp <1129597198@qq.com>
2017-04-19 08:47:45 +0800

    1

---
+++ Touha.Infrastructure.Utils/Graphics/GraphicUtil.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Touha.Infrastructure.Utils.Graphics
+{
+    public class GraphicUtil
+    {
+        /// <summary>
+        /// 判断点是否在多边形内
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="polygon"></param>
+        /// <returns></returns>
+        public bool IsPointInPolygon(Point p, Point[] polygon)
+        {
+            double minX = polygon[0].X;
+            double maxX = polygon[0].X;
+            double minY = polygon[0].Y;
+            double maxY = polygon[0].Y;
+            for (int i = 1; i < polygon.Length; i++)
+            {
+                Point q = polygon[i];
+                minX = Math.Min(q.X, minX);
+                maxX = Math.Max(q.X, maxX);
+                minY = Math.Min(q.Y, minY);
+                maxY = Math.Max(q.Y, maxY);
+            }
+
+            if (p.X < minX || p.X > maxX || p.Y < minY || p.Y > maxY)
+            {
+                return false;
+            }
+
+            // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+            bool inside = false;
+            for (int i = 0, j = polygon.Length - 1; i < polygon.Length; j = i++)
+            {
+                if ((polygon[i].Y > p.Y) != (polygon[j].Y > p.Y) &&
+                     p.X < (polygon[j].X - polygon[i].X) * (p.Y - polygon[i].Y) / (polygon[j].Y - polygon[i].Y) + polygon[i].X)
+                {
+                    inside = !inside;
+                }
+            }
+
+            return inside;
+        }
+    }
+}
---
+++ Touha.Infrastructure.Utils/System/SystemHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Touha.Infrastructure.Utils.System
+{
+    public class SystemHelper
+    {
+        private static OperatingSystem _os = Environment.OSVersion;
+
+        public static string GetServicePack()
+        {
+            return _os.ServicePack;
+        }
+
+        public static string GetVersionString()
+        {
+            return _os.VersionString;
+        }
+
+        public static string GetPaltform()
+        {
+            return _os.Platform.ToString();
+        }
+    }
+}
